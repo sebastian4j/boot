9330942462e94c03c96a7a1f26236462a3139017
Sebastian Avila A <sebastian4j@gmail.com>
2018-11-29 22:55:04 -0300

    obtener los beans del contexto

---
+++ boot-uno/src/main/java/com/sebastian/boot/uno/Main.java
@@ -0,0 +1,30 @@
+package com.sebastian.boot.uno;
+
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * obtener todos los beans del contexto.
+ * 
+ * @author Sebastian Avila A.
+ *
+ */
+@SpringBootApplication
+public class Main implements CommandLineRunner {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+	public static void main(String[] args) {		
+		var ctx = SpringApplication.run(Main.class, args);
+		LOGGER.info("total de beans: {}", ctx.getBeanDefinitionCount());
+		Arrays.asList(ctx.getBeanDefinitionNames()).forEach(l -> LOGGER.info(l));
+	}
+	
+	@Override
+    public void run(String...args) throws Exception {
+		LOGGER.info("ejecutado despues del SpringApplication.run");
+    }
+}
